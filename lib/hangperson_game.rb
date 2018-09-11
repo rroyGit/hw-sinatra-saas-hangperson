834afc4c57b4dd6a17d685f05c7f76d4ed1fd19f
Rupam Roy <rroy6@binghamton.edu>
2018-09-11 00:36:56 +0000

    Continuing testing

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,12 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -23,5 +27,10 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
+  
+  def guess aGuess
+    @guesses = aGuess
+  end
+  
+  
 end
